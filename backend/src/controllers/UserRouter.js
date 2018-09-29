0d52838852b55b3f8d3b06a27b846dd60565c9ea
Johnny Edgett <bostonjohn94@gmail.com>
2018-09-29 18:49:42 -0500

    testing out posting

--- backend/src/controllers/UserRouter.js
+++ backend/src/controllers/UserRouter.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+// const bodyParser = require('body-parser')
+var getConnection = require('./utils/DBConnector')
+
 // var User = require('../models/UserSchema')
 
 router.get('/', (req, res) => {
@@ -10,4 +13,19 @@ router.get('/:id', (req, res) => {
   res.send('You asked for: ' + req.params.id)
 })
 
+// Just testing out posting data to user
+router.post('/createUser', (req, res) => {
+  var user = req.body.username
+  getConnection((err, con) => {
+    if (err) res.send(err)
+    else {
+      var query = `INSERT INTO Users (username) VALUES('${user}')`
+      con.query(query, (err, results) => {
+        if (err) throw err
+        con.release()
+        res.send(results)
+      })
+    }
+  })
+})
 module.exports = router
